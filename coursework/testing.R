c69b8b33f822834a7361edf3b0c02840717d8bfc
zattri <daninjakidy@gmail.com>
2022-04-04 15:03:27 +0100

    Added new objective function and integrated with testing environment

--- coursework/testing.R
+++ coursework/testing.R
@@ -1,4 +1,6 @@
 library("plyr")
+setwd("C:/Users/Zattri/Desktop/modern_opt/coursework")
+
 # Testing transforming data to give sector price/earnings sum
 df = read.csv("financials.csv")
 trouble_makers = df[rowSums(is.na(df)) != 0,] # Find the companies with null values
@@ -19,12 +21,29 @@ sum_price_per_earnings = function(data, weightings){
     if (w > 0) {
       sector = as.double(which(weightings == w))
       sector_data = data[data$sector_index == sector,]
-      sector_prices = sector_data$Price.Earnings
-      total_sum = total_sum + (sum(sector_prices) * w)
+      price_earnings = sector_data$Price.Earnings
+      total_sum = total_sum + (sum(price_earnings) * w)
     }
   }
   
   return(total_sum)
+}
+
+
+sum_value_at_risk = function(data, weightings) {
+  value_at_risk = 0
+  for (w in weightings) {
+    if (w > 0) {
+      sector = as.double(which(weightings == w))
+      sector_data = data[data$sector_index == sector,]
+      week_high = sector_data$X52.Week.High
+      price = sector_data$Price
+      risk_value = sum(price) - sum(week_high)
+      value_at_risk = value_at_risk + (risk_value * w)
+    }
+  }
+  
+  return(value_at_risk)
 }
 
 generateWeighting = function(groups = 11) {
@@ -36,20 +55,19 @@ generateWeighting = function(groups = 11) {
 
 #weightings = c(0,0,0.3,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0) - Example weighting
 
-lowest_price <<- 10000000000
-for (i in 1:100) {
+lowest_price = 10000000000
+for (i in 1:10000) {
   weightings = generateWeighting()
-  calculated_price = sum_price_per_earnings(data=df, weightings=weightings)
-  if (calculated_price <= lowest_price) {
-    best_weighting <<- weightings
-    lowest_price <<- calculated_price
-    print(lowest_price)
+  cost = sum_price_per_earnings(data=df, weightings=weightings)
+  VaR = sum_value_at_risk(data=df, weightings=weightings)
+  profits = cost + VaR
+  if (profits <= lowest_price) {
+    best_weighting = weightings
+    lowest_price = profits
   }
 }
-# Not updating the lowest price properly
 
-# What a great hack, absolute genius, can't believe it, giga chad
-
+print(cat("Best weights:", best_weighting, "|", lowest_price))
 
 
 # Stolen from Atif
